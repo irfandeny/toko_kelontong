0621d6c2314876103c1070886ffc0b1b77dce683
Irfan Deny <irfandeny112@gmail.com>
2025-11-25 13:29:49 +0700

    add kategoti

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'purchase_price',
+        'selling_price',
+        'stock',
+        'unit',
+        'image',
+        'description'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function purchaseDetails()
+    {
+        return $this->hasMany(PurchaseDetail::class);
+    }
+
+    public function saleDetails()
+    {
+        return $this->hasMany(SaleDetail::class);
+    }
+}
